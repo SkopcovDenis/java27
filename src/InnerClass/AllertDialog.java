6417fe52a23ed0685625f6ebf4f8ac97578d2cb2
SkopcovDenis <vocpaks@gmail.com>
2022-05-18 00:56:07 +0300

    Новые задания

---
+++ src/InnerClass/AllertDialog.java
@@ -0,0 +1,18 @@
+package InnerClass;
+
+public class AllertDialog {
+
+    static class Builder {
+
+        public Builder(Object o) {
+        }
+
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        AllertDialog.Builder builder = new AllertDialog.Builder(null);
+
+    }
+}
---
+++ src/lesson19/GennericTest.java
@@ -0,0 +1,13 @@
+package lesson19;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GennericTest {
+    public static void main(String[] args) {
+
+        List<Object> strings = new ArrayList<>();
+
+
+    }
+}
